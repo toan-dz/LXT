1206f6eb14a5d11c06216afc4cfbb060593a6c29
ltoan5861 <80272006+toan-dz@users.noreply.github.com>
2024-04-08 17:56:37 +0700

    Add files via upload

---
+++ WebApplication01/WebApplication01/Controllers/Home01Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication01.Controllers
+{
+    public class Home01Controller : Controller
+    {
+        // GET: Home01
+        public ActionResult Index()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Chào mừng bạn đến với ASP.NET MVC 5";
+            return View();
+        }
+        //GET: /Home01/About
+        public ActionResult About()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Đây là trang About";
+            return View();
+        }
+    }
+}
